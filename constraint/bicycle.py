422216f7108a95c66ccac31a628e02ddbc678266
StefenYin <zqyin@ucdavis.edu>
2012-12-15 23:39:13 -0800

    Built the lambda_from_132 function in bicycle.py, and Tested the function in test_bicycle.py.

--- constraint/bicycle.py
+++ constraint/bicycle.py
@@ -1,5 +1,6 @@
-from numpy import pi, sin, cos, tan
+from numpy import pi, sin, cos, tan, arctan
 import numpy as np
+from scipy.optimize import newton
 
 def benchmark_parameters():
     """Returns the benchmark bicycle parameters from [Meijaard2007]_."""
@@ -130,3 +131,38 @@ def benchmark_to_moore(benchmarkParameters):
     mP['g'] = bP['g']
 
     return mP
+
+def lambda_from_132(rF, rR, d1, d3, d2):
+    '''Returns the steer axis tilt, lamba, for the parameter set based on the
+    offsets from the steer axis.
+
+    Parameters
+    ----------
+    rF : float
+        Front wheel radius.
+    rR : float
+        Rear wheel radius.
+    d1 : float
+        The rear wheel offset from the steer axis.
+    d3 : float
+        The front wheel offset from the steer axis.
+    d2 : float
+        The distance along the steer axis between the front wheel and rear
+        wheel.
+
+    Returns
+    -------
+    lam : float
+        The steer axis tilt as described in Meijaard2007.
+
+    '''
+    def lam_equality(lam, rF, rR, d1, d3, d2):
+        return sin(lam) - (rF - rR + d2 * cos(lam)) / (d1 + d3)
+
+    guess = arctan(d2 / (d1 + d3)) # guess based on equal wheel radii
+
+    args = (rF, rR, d1, d3, d2)
+
+    lam = newton(lam_equality, guess, args=args)
+
+    return lam
--- constraint/test_bicycle.py
+++ constraint/test_bicycle.py
@@ -3,7 +3,12 @@
 ##############
 
 import bicycle as bi
-from dtk import bicycle as bicycle
+#from dtk import bicycle as bicycle
 
 bp = bi.benchmark_parameters() #assertation bicycle.benchmark_parameters
 mp = bi.benchmark_to_moore(bp) #assertation bicycle.benchmark_to_moore
+
+lam = bi.lambda_from_132(mp['rf'], mp['rr'], mp['d1'], mp['d3'], mp['d2'])
+#assertation bicycle.lambda_from_abc
+
+
