f7b2dd8abef6ae36c586a5c5dff493fd6df8461d
StefenYin <zqyin@ucdavis.edu>
2012-12-16 15:12:32 -0800

    Updated the test using zeros of lean and steer angles.

--- constraint/test_bicycle.py
+++ constraint/test_bicycle.py
@@ -12,7 +12,7 @@ mp = bi.benchmark_to_moore(bp) #assertation bicycle.benchmark_to_moore
 lam = bi.lambda_from_132(mp['rf'], mp['rr'], mp['d1'], mp['d3'], mp['d2'])
 #assertation bicycle.lambda_from_abc
 
-lean = pi/8; steer = pi/4
+lean = 0.0; steer = 0.0
 pitch = bi.pitch_from_roll_and_steer(lean, steer, mp['rf'], mp['rr'], 
                                 mp['d1'], mp['d2'], mp['d3'])
 #assertation bicycle.pitch_from_roll_and_steer(lean, steer, mp['rf'], mp['rr'], 
