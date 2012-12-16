fc44e2c76c1ac0dbb4237e727d7507636d55868c
StefenYin <zqyin@ucdavis.edu>
2012-12-15 23:20:22 -0800

    Built bicycle test file, now is to test the first built two functions: benchmark_parameters, and benchmark_to_mooor.

---
+++ constraint/test_bicycle.py
@@ -0,0 +1,9 @@
+##############
+# test bicycle
+##############
+
+import bicycle as bi
+from dtk import bicycle as bicycle
+
+bp = bi.benchmark_parameters() #assertation bicycle.benchmark_parameters
+mp = bi.benchmark_to_moore(bp) #assertation bicycle.benchmark_to_moore
