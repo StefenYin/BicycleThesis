941de5d7d6b82d21cc6e9bf42e24672032ab16ba
StefenYin <zqyin@ucdavis.edu>
2012-12-18 10:08:20 -0800

    Updated coding convention of test_bicycle.py module.

--- constraint/test_bicycle.py
+++ constraint/test_bicycle.py
@@ -3,17 +3,21 @@
 ##############
 
 import bicycle as bi
+
 from numpy import pi
 #from dtk import bicycle as bicycle
 
+
 #===============
 #parameters test
+
 bp = bi.benchmark_parameters() #assertation bicycle.benchmark_parameters
 mp = bi.benchmark_to_moore(bp) #assertation bicycle.benchmark_to_moore
 
 
 #==========
 #pitch test
+
 lam = bi.lambda_from_132(mp['rf'], mp['rr'], mp['d1'], mp['d3'], mp['d2'])
 #assertation bicycle.lambda_from_abc
 
@@ -27,7 +31,6 @@ pitch = bi.pitch_from_roll_and_steer(lean, steer, mp['rf'], mp['rr'],
 #=========
 #basu test
 
-#input
 basu_input = bi.basu_table_one_input()
 stefen_input = bi.basu_to_stefen_input(basu_input, mp['rr'], bp['lambda'])
 #assertation bicycle.basu_to_moore_input
