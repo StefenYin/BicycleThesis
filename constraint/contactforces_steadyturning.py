d95b3cfa1a68da0b0c6c287ec9ee7e4b81a95b63
StefenYin <zqyin@ucdavis.edu>
2013-01-08 15:39:16 -0800

    Built a function eigenvalues in SteadyTurning class in order to check the stability of a given steady turning configuration. Now the configuration need to pass three steps: one is turning radius, two is equilibrium values, three is eigenvalues.

--- constraint/contactforces_steadyturning.py
+++ constraint/contactforces_steadyturning.py
@@ -4,6 +4,9 @@ contactforces_steadyturning module:
    position;
 2, & dynamic equations, nonholonomic equations -> equilibrium values, 
    contact forces.
+3, Equilibrium values checks, encompassing 1) turning radius check; 2) rear
+   wheel rate unsolvable; 3) rear wheel rate solved to have imaginary values.
+   4) stability of equilibrium values in the defined configuration.
 """
 
 import model as mo
@@ -41,6 +44,8 @@ class SteadyTurning(object):
         # individual centers of mass of four rigid bodies
         biModel = mo.BicycleModel()
 
+        self._mmFull = biModel.mass_matrix_full()
+        self._forcingLinA = biModel.linearized_a()
         self._forceFull = biModel.forcing_full()
         self._contactforcesOrig = biModel.contact_forces()
         self._contactPosition = (biModel._contact_posi_pq + 
@@ -108,7 +113,7 @@ class SteadyTurning(object):
         try:
             u5_value = sym.solve(self._dynamicnonho[0], u5)[0]
         except:
-            raise ValueError("Oops! Rear wheel rate in configuration {0} cannot be \
+            raise ValueError("\nOops! Rear wheel rate in configuration {0} cannot be \
 solved. Please select valid configuration according to the plot from <General \
 steady turning of a benchmark bicycle model> by Luke. One way you can check \
 what the equilibrium values will look like is to see the dynamic_nonho_equ by \
@@ -116,9 +121,13 @@ removing self.equi_cal() in Class and running again to see the \
 self._dynamicnonho. Good luck!".format(self._configuration))
 
         if complex(u5_value).real == 0.:
-            raise ValueError("Oops! The steady turning in your configuration \
-{0} is Infeasible. Please try another valid according to the plot from <General \
-steady turning of a benchmark bicycle model> by Luke.".format(self._configuration))
+            print ("\nOops! The steady turning in your configuration {0} seems \
+Infeasible since no real value appears in rear wheel rate. Please check the \
+_equilibrium_u values and try another valid one according to the plot from \
+<General steady turning of a benchmark bicycle model> by Luke.".format(self._configuration))
+        else:
+            print ("\nIt passed the check of equilibrium calculation and \
+already solved the equilibrium values, but it is still being checked...")
 
         u_others_dict = {u5: u5_value}
 
@@ -129,7 +138,8 @@ steady turning of a benchmark bicycle model> by Luke.".format(self._configuratio
 
         u_others_dict.update(dict(zip([u1, u6, T4], 
                                       [u1_value, u6_value, T4_value])))
-        self.equilibrium = u_others_dict
+        self._equilibrium_u = u_others_dict
+        self._equilibrium_u.update(self._u0s)
 
 
     def contact_force(self):
@@ -137,7 +147,7 @@ steady turning of a benchmark bicycle model> by Luke.".format(self._configuratio
         equilibrium values. 
         The contact forces are expressed by each body-fixed coordinates."""
 
-        equilibrium = self.equi_cal()
+        equilibrium = self._equilibrium_u
 
         contact_forces_st = [value.subs(self._ud0s).subs(self._u0s)
                                 .subs(self._parameters)
@@ -191,12 +201,44 @@ steady turning of a benchmark bicycle model> by Luke.".format(self._configuratio
                                 self._turningRadiusSym)
         
         if turn_radius == []:
-            print ("It seems the configuration {0} that you are building is \
+            print ("\nIt seems the configuration {0} that you are building is \
 not going to generate a steady turning. Maybe you need to try another valid \
 configuration.".format(self._configuration))
             pass
         else:
+            print ("\nIt seems the configuration {0} that you are building is \
+right. At lease it has turning radius at this point, but it is still being \
+checked...".format(self._configuration))
+
             self._turningRadiusRearGeo = turn_radius[Rr]
             self._turningRadiusFrontGeo = turn_radius[Rf]
             self._turningRadiusCom = sqrt(float(self._totalComA123[0]**2 + 
                                     (turn_radius[Rr] - self._totalComA123[1])**2))
+
+
+    def eigenvalues(self):
+        """Returns the eigenvalues of state A matrix in the defined steady
+        turning configuration."""
+
+        self._equilibrium_qu = {}
+        self._equilibrium_qu.update(self._configuration)
+        self._equilibrium_qu.update(self._equilibrium_u)
+
+        mass = self._mmFull.subs(self._parameters).subs(self._equilibrium_qu)
+        lin_forcing_a = self._forcingLinA.subs(self._parameters).subs(self._equilibrium_qu)
+        self._AmatFull = mass.inv() * lin_forcing_a
+        self._Amat = self._AmatFull.extract([4,5,6], [3,4,5])
+        eig1, eig2, eig3 = self._Amat.eigenvals(rational = False)
+
+        self._eigs = [complex(eigvalue) for eigvalue in [eig1, eig2, eig3]]
+
+        ei_signs = [eigs.real <= 0. for eigs in self._eigs]
+
+        if False not in ei_signs:
+            print ("\nIt passed the eigenvalues checking. You can see the \
+eigenvalues in ._eigs. Overall, the configuration {0} seems already generates \
+a steady turning.".format(self._configuration))
+        else:
+            print ("\nSome eigenvalues are positive which means the steady \
+turning in the configuration {0} is not stable or kinematically infeasible. \
+Please check the eigenvalue and try another configuration.".format(self._configuration))
